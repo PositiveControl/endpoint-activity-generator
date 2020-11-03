40991f8d7390396a17f13890a6318dc17bc91dfc
Mark Evans <msteven.evans@gmail.com>
2020-11-03 16:18:30 -0700

    Creates file and logs creation

--- eag.rb
+++ eag.rb
@@ -7,27 +7,56 @@ class EAG
     @path = path
     @user = Etc.getlogin
     @pid = nil
+    @action = nil
+    @command_line = nil
+    @file_path = nil
   end
 
   def start_process
-    stdin, stdout, thread = Open3.popen2(@path)
-    @pid = thread.pid
-    log_activity
-    puts stdout.read
-    [stdin, stdout].each(&:close)
+    @action = "start_process"
+    @command_line = @path
+    execute_command
+  end
+
+  def create_file(name, extension)
+    @action = "create"
+    @file_path = File.expand_path(@path + name + extension)
+    @command_line = "touch #{@file_path}"
+    execute_command
   end
 
   private
 
+  def execute_command
+    stdin, stdout, thread = Open3.popen2(@command_line)
+    @pid = thread.pid
+    log_activity
+    # puts is only for spec expectation
+    puts stdout.read
+    [stdin, stdout].each(&:close)
+  end
+
   def process_name
+    # "/" wont work on Windows
     @path.split("/").last
   end
 
+  def file_operations
+    # set as constants
+    ["create", "update", "delete"]
+  end
+
+  # pull out into module and include
   def log_activity
     file = File.open('logs/activity.csv', File::WRONLY | File::APPEND | File::CREAT)
     logger = Logger.new(file)
     logger.formatter = proc do |_, datetime, _, msg|
-        "timestamp: #{datetime.to_s},user: #{@user},process_name: #{process_name},command_line: #{msg},pid: #{@pid}\n"
+      if file_operations.include?(@action)
+        # process_name - do we want the script's process name, or process_name of the executed command?
+        "timestamp: #{datetime.to_s},action: #{@action}, file_path: #{@file_path},user: #{@user},process_name: #{$PROGRAM_NAME},command_line: #{@command_line},pid: #{@pid}\n"
+      else
+        "timestamp: #{datetime.to_s},action: #{@action},user: #{@user},process_name: #{process_name},command_line: #{@command_line},pid: #{@pid}\n"
+      end
     end
     logger.info(@path)
     logger.close
--- spec/eag_spec.rb
+++ spec/eag_spec.rb
@@ -3,10 +3,13 @@ require 'csv'
 require 'pry'
 
 RSpec.describe EAG do
-  before do
+  before(:all) do
     # make forrest executable
     system %(chmod +x spec/concerns/forrest)
   end
+
+  let(:username) { Etc.getlogin }
+  let(:logs) { File.read('./logs/activity.csv') }
 
   context 'starting processes' do
     it 'should start a process given a path' do
@@ -16,13 +19,54 @@ RSpec.describe EAG do
     end
   end
 
-  context 'logging activity' do
+  context 'file operations' do
+    after do
+      # cleanup created file
+      system %(rm ./spec/concerns/nefarious.txt)
+    end
+
+    it 'creates a new file given a path' do
+      EAG.new('./spec/concerns/').create_file('nefarious','.txt')
+      # sleep to allow expectation to finish before after block is called
+      sleep(0.1)
+
+      expect(File.exist?("./spec/concerns/nefarious.txt")).to be_truthy
+    end
+  end
+
+  context 'logging process start' do
     it 'should log user who started the process' do
-      username = Etc.getlogin
       EAG.new('./spec/concerns/forrest').start_process
-      logs = File.read('./logs/activity.csv')
 
       expect(logs.include?(username)).to be_truthy
+    end
+  end
+
+  context 'logging file operations'do
+    before(:all) do
+      EAG.new('./spec/concerns/').create_file('nefarious','.txt')
+    end
+
+    after(:all) do
+      sleep(0.1)
+      system %(rm ./spec/concerns/nefarious.txt)
+    end
+
+    xit 'logs process name' do
+      # process of script, or process of executed command line?
+      expect(logs.include?("file_create")).to be_truthy
+    end
+
+    xit 'logs full file path' do
+      # expect(logs.include?("")).to be_truthy
+    end
+
+    it 'logs file name' do
+      expect(logs.include?("nefarious.txt")).to be_truthy
+    end
+
+    it 'logs activity descriptor' do
+      expect(logs.include?("action: create")).to be_truthy
     end
   end
 end
