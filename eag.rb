bd9e023fbbf26a2b0d7524fa3f7067b973cf373b
Mark Evans <msteven.evans@gmail.com>
2020-11-03 01:11:02 -0700

    Fixes wrong PID and process blocking logging issues

--- eag.rb
+++ eag.rb
@@ -1,21 +1,35 @@
 require 'logger'
+require 'etc'
+require 'open3'
 
-module EAG
-  def self.start_process(command)
-    pid = spawn(command)
-    Process.wait(pid)
-    log_activity(Etc.getlogin, pid, command)
+class EAG
+  def initialize(path)
+    @path = path
+    @user = Etc.getlogin
+    @pid = nil
+  end
+
+  def start_process
+    stdin, stdout, thread = Open3.popen2(@path)
+    @pid = thread.pid
+    log_activity
+    puts stdout.read
+    [stdin, stdout].each(&:close)
   end
 
   private
 
-  def self.log_activity(user, pid, command)
+  def process_name
+    @path.split("/").last
+  end
+
+  def log_activity
     file = File.open('logs/activity.csv', File::WRONLY | File::APPEND | File::CREAT)
     logger = Logger.new(file)
     logger.formatter = proc do |_, datetime, _, msg|
-        "timestamp: #{datetime.to_s},user: #{user},process_name: #{$PROGRAM_NAME},command_line: #{msg},pid: #{pid}\n"
+        "timestamp: #{datetime.to_s},user: #{@user},process_name: #{process_name},command_line: #{msg},pid: #{@pid}\n"
     end
-    logger.info(command)
+    logger.info(@path)
     logger.close
   end
 end
--- spec/eag_spec.rb
+++ spec/eag_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe EAG do
 
   context 'starting processes' do
     it 'should start a process given a path' do
-      expect { EAG.start_process('./spec/concerns/forrest') }
+      expect { EAG.new('./spec/concerns/forrest').start_process }
         .to output(a_string_including("Look ma!  I'm running!"))
           .to_stdout_from_any_process
     end
@@ -19,7 +19,7 @@ RSpec.describe EAG do
   context 'logging activity' do
     it 'should log user who started the process' do
       username = Etc.getlogin
-      EAG.start_process('./spec/concerns/forrest')
+      EAG.new('./spec/concerns/forrest').start_process
       logs = File.read('./logs/activity.csv')
 
       expect(logs.include?(username)).to be_truthy
